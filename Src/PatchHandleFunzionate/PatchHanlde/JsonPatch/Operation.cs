5afade00e63dcd6e74a591f4159d67520db919d6
Raf <raffaeler@vevy.com>
2021-12-23 14:59:38 +0100

    Small change to avoid boxing

--- Src/PatchHandleFunzionate/PatchHanlde/JsonPatch/Operation.cs
+++ Src/PatchHandleFunzionate/PatchHanlde/JsonPatch/Operation.cs
@@ -9,11 +9,11 @@ namespace PatchHanlde.JsonPatch
         public string Path { get; set; }
         public JsonElement Value { get; set; }
 
-        private static Dictionary<string, object> _actions = new();
+        private static Dictionary<string, object?> _actions = new();
 
         public void Patch<T>(T model)
         {
-            if (!Enum.TryParse<OperationType>(Op, true, out OperationType operationType))
+            if (!Enum.TryParse(Op, true, out OperationType operationType))
                 return;
 
             switch (operationType)
@@ -36,12 +36,15 @@ namespace PatchHanlde.JsonPatch
 
         private Action<T, Operation> CreateDelegate<T>()
         {
-            if (_actions.TryGetValue(typeof(T).FullName, out object action))
+            // modified so that the cache is also tied to the path
+            var key = $"{typeof(T).FullName}.{Path}";
+
+            if (_actions.TryGetValue(key, out object? action))
                 return (Action<T, Operation>)action;
 
             var expression = CreateReplaceExpression<T>();
             var del = expression.Compile();
-            _actions[typeof(T).FullName] = del;
+            _actions[key] = del;
             return del;
         }
 
@@ -69,15 +72,45 @@ namespace PatchHanlde.JsonPatch
 
             var value = Expression.Call(operation, getValueMethod, Expression.Constant(propertyInfo.PropertyType));
 
+            var propType = Expression.Constant(propertyInfo.PropertyType);
+            var valueProp = Expression.Property(operation, "Value");
+            var getValue = GetGetValueExpression(valueProp, propertyInfo.PropertyType);
+
             //var assignment = Expression.Assign(accessProperty, 
             //    Expression.Convert(
             //        Expression.Constant(GetValue(propertyInfo.PropertyType, (JsonElement)Value)), propertyInfo.PropertyType));
-            var assignment = Expression.Assign(accessProperty, 
-                Expression.Convert(
-                    value, propertyInfo.PropertyType));
+
+            //var assignment = Expression.Assign(accessProperty, 
+            //    Expression.Convert(
+            //        value, propertyInfo.PropertyType));
+
+            var assignment = Expression.Assign(accessProperty, getValue);
 
             var lambda = Expression.Lambda<Action<T, Operation>>(assignment, input, operation);
             return lambda;
+        }
+
+        private Expression GetGetValueExpression(Expression jsonElement, Type type)
+        {
+            Type jet = typeof(JsonElement);
+            if (type == typeof(Guid)) return Expression.Call(jsonElement, jet.GetMethod("GetGuid"));
+            if (type == typeof(string)) return Expression.Call(jsonElement, jet.GetMethod("GetString"));
+            if (type == typeof(bool)) return Expression.Call(jsonElement, jet.GetMethod("GetBoolean"));
+            if (type == typeof(byte)) return Expression.Call(jsonElement, jet.GetMethod("GetByte"));
+            if (type == typeof(sbyte)) return Expression.Call(jsonElement, jet.GetMethod("GetSByte"));
+            if (type == typeof(DateTime)) return Expression.Call(jsonElement, jet.GetMethod("GetDateTime"));
+            if (type == typeof(DateTimeOffset)) return Expression.Call(jsonElement, jet.GetMethod("GetDateTimeOffset"));
+            if (type == typeof(Decimal)) return Expression.Call(jsonElement, jet.GetMethod("GetDecimal"));
+            if (type == typeof(Double)) return Expression.Call(jsonElement, jet.GetMethod("GetDouble"));
+            if (type == typeof(Single)) return Expression.Call(jsonElement, jet.GetMethod("GetSingle"));
+            if (type == typeof(Int16)) return Expression.Call(jsonElement, jet.GetMethod("GetInt16"));
+            if (type == typeof(Int32)) return Expression.Call(jsonElement, jet.GetMethod("GetInt32"));
+            if (type == typeof(Int64)) return Expression.Call(jsonElement, jet.GetMethod("GetInt64"));
+            if (type == typeof(UInt16)) return Expression.Call(jsonElement, jet.GetMethod("GetUInt16"));
+            if (type == typeof(UInt32)) return Expression.Call(jsonElement, jet.GetMethod("GetUInt32"));
+            if (type == typeof(UInt64)) return Expression.Call(jsonElement, jet.GetMethod("GetUInt64"));
+
+            throw new NotSupportedException($"The type {type.Name} is not supported");
         }
 
         private object? GetValue(Type propertyType)
@@ -114,6 +147,7 @@ namespace PatchHanlde.JsonPatch
 
             return Path.Substring(1);
         }
+
 
     }
 }
